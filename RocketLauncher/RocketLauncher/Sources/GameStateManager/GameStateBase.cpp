c2b244470e8f94ebcea81221a8ff35e60bc6fc47
S4ltF1sh <sankatana100@gmail.com>
2022-02-22 11:39:25 +0700

    Fix

---
+++ RocketLauncher/RocketLauncher/Sources/GameStateManager/GameStateBase.cpp
@@ -0,0 +1,33 @@
+#include "GameStateBase.h"
+#include "GSIntro.h"
+#include "GSMenu.h"
+#include "GSPlay.h"
+#include "GSEnd.h"
+#include "GSWin.h"
+
+GameStateBase* GameStateBase::CreateState(StateTypes st) {
+	GameStateBase* gs = nullptr;
+	switch (st)
+	{
+	case INVALID:
+		break;
+	case INTRO:
+		gs = new GSIntro();
+		break;
+	case MENU:
+		gs = new GSMenu();
+		break;
+	case PLAY:
+		gs = new GSPlay();
+		break;
+	case END:
+		gs = new GSEnd();
+		break;
+	case WIN:
+		gs = new GSWin();
+		break;
+	default:
+		break;
+	}
+	return gs;
+}
--- Wibu time!/Wibu time!/Sources/GameObjects/IMap.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "IMState.h"
-#include "HitBox.h"
-
-class IMap
-{
-public:
-	virtual void changeNextState(IMState::STATE nextState) = 0;
-};
